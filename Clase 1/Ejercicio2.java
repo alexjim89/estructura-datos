3b6f8a7a148135f1d91098ec966539b80eaa6965
alexjim89 <alexjim_89_@outlook.com>
2021-07-14 15:11:24 -0500

    m

---
+++ Clase 1/Ejercicio2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ejercicio2 {
+     
+         public static void main(String arg[]){
+        Scanner sc = new Scanner (System.in);
+        String nombre = "";
+        System.out.println("Digita nombre del alumno");
+        nombre = sc.nextLine();
+        System.out.println("Digita resultado de matematicas");
+        float matematicas = sc.nextFloat();
+        System.out.println("Digita resultado de ingles");
+        float ingles = sc.nextFloat();
+        System.out.println("Digita resultado de sociales");
+        Float sociales = sc.nextFloat();
+        float promedio = (matematicas + ingles + sociales) / 3;
+        
+        if (promedio >= 3.0){
+            System.out.println("El alumno " + nombre + " aprobo con promedio de: " + promedio);
+        } else {
+            System.out.println("El alumno " + nombre + " reprobo con promedio de: " + promedio);
+        }
+          }
+}
