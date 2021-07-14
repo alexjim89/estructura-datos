b81cfe5d726dcd3b02aee90adef3f0c687232e07
alexjim89 <alexjim_89_@outlook.com>
2021-07-14 15:36:02 -0500

    m

---
+++ Clase 1/Ejercicio4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Introduce un número entero:");                                                         
+        n = sc.nextInt();
+        System.out.println("Tabla del " + n);
+        for(int i = 1; i<=10; i++){
+             System.out.println(n + " * " + i + " = " + n*i);                                                     
+        }
+    }
+}
