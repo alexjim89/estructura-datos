272c121c08294e43a29ca60c8f99abbade6e18db
alexjim89 <alexjim_89_@outlook.com>
2021-07-14 15:26:01 -0500

    m

---
+++ Clase 1/Ejercicio3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ejercicio3 {
+     
+         public static void main(String arg[]) {
+        Scanner sc = new Scanner (System.in);
+        String nombre = "";
+        System.out.println("Digita nombre del trabajador");
+        nombre = sc.nextLine();
+        System.out.println("Digita el numero de horas trabajadas este mes");
+        int horas = sc.nextInt();
+        int pago = (horas * 30000);
+        System.out.println("El trabajador " + nombre + " recibe un salario de: " + pago);
+     }
+}
