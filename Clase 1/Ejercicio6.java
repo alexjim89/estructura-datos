d93d63d5931b83459d833ca5b8e886ed0a43dab4
alexjim89 <alexjim_89_@outlook.com>
2021-07-14 20:34:35 -0500

    m

---
+++ Clase 1/Ejercicio6.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Ejercicio6 {
+     
+         public static void main(String arg[]) {
+        Scanner sc = new Scanner (System.in);
+        String genero = "h";
+        System.out.println("Digita genero de la persona h/m");
+        genero = sc.nextLine();
+        
+        if (genero == "h"){
+            System.out.println("Digita el peso de la persona en kilos");
+            float peso = sc.nextInt();
+            System.out.println("Digita la altura de la persona en centimetros");
+            float altura = sc.nextInt();
+            float ideal = (altura - 110);
+            System.out.println("Tu peso ideal es " + ideal);
+            
+        }else {
+            System.out.println("Digita el peso de la persona en kilos");
+            float peso = sc.nextInt();
+            System.out.println("Digita la altura de la persona en centimetros");
+            float altura = sc.nextInt();
+            float ideal = (altura - 120);
+            System.out.println("Tu peso ideal es " + ideal);
+        }
+        
+     }
+}
