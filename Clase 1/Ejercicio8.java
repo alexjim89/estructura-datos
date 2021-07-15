0a20b72450518cb83537ed09a55fcbcd3b6c1d24
alexjim89 <alexjim_89_@outlook.com>
2021-07-14 23:38:28 -0500

    m

--- Clase 1/Ejercicio8.java
+++ Clase 1/Ejercicio8.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+import java.util.stream.Stream;
 public class Ejercicio8 {
    public static void main(String args[]){
       Scanner sc = new Scanner(System.in);
       int compu = (int)(Math.random() * 3) + 1;
       System.out.println("La computadora ya escogio...");
-      System.out.println("Indique su eleccion 1: Piedra, 2: Papel, 3: Tijera:");
+      System.out.println("Indique su eleccion p: Piedra, l: Papel, t: Tijera:");
       int Usuario = sc.nextInt();
       System.out.println("La computadora habia escogido:");
       switch (compu){
