727c19dd94e01e30dc0f797463e34a4ff5a2e4b4
Alexander Jimenez <alexjim_89_@outlook.com>
2021-08-02 15:30:50 -0500

    m

---
+++ Clase 1/Proyecto1/src/App16.java
@@ -0,0 +1,16 @@
+public class App16 {
+    public static void main(String[] args){
+        int nums[] = new int[5];
+        nums[0]= 5;
+        nums[1]= 220;
+        nums[2]= 8;
+        nums[3]= 458;
+        nums[4]= 22;
+
+        System.out.print("[" + nums [0] + "]");
+        System.out.print("[" + nums [1] + "]");
+        System.out.print("[" + nums [2] + "]");
+        System.out.print("[" + nums [3] + "]");
+        System.out.print("[" + nums [4] + "]");
+    }
+}
---
+++ Clase 1/Proyecto1/src/App17.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class App17 {
+    public static void main(String[] args){
+        int longi = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("cuantos numeros desea ingresar: ");
+        longi =sc.nextInt();
+        int nums[] = new int[longi];
+        for(int i = 0; i < nums.length; i++){
+            System.out.print("dame el valor # " + (i+1) + ": ");
+            nums [i] = sc.nextInt();
+        }for (int i = 0; i < nums.length; i++){
+            System.out.print("[" + nums[i] + "]");
+        }
+        sc.close();
+    }
+}
