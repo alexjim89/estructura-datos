bb36ed0c5881ebe72b5c6cb0d2ca47b85a081fac
Alexander Jimenez <alexjim_89_@outlook.com>
2021-08-02 14:42:52 -0500

    m

---
+++ Clase 1/Proyecto1/src/App15.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class App15 {
+    public static void main(String[] args){
+        String cadenaoriginal = "", cadenasecundaria = "";
+        int numcarac = 0, desde = 0, hasta = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa cadena de caracteres: ");
+        cadenaoriginal = sc.nextLine();
+        numcarac = cadenaoriginal.length();
+        System.out.println("la cadena de caracteres " + cadenaoriginal + " posee " + numcarac + " caracteres.");
+        System.out.print("Desde que caracter deseas obtener la nueva cadena: ");
+        desde = sc.nextInt();
+        System.out.print("Hasta que caracter deseas obtener la nueva cadena: ");
+        hasta = sc.nextInt();
+        cadenasecundaria = cadenaoriginal.substring(desde, hasta);
+        System.out.println("la nueva cadena es " + cadenasecundaria);
+        sc.close();
+    }
+}
